fc3da910b46a94438d4ed80a87dcc716b9d1a1f4
Ádám <adam@lenartadam.hu>
2023-10-07 22:21:12 +0200

    This is a project base.

--- Programozás alapjai/Lesson 4/Task1.py
+++ Programozás alapjai/Lesson 4/Task1.py
@@ -1,1 +1,5 @@
-print("123")
+names = ["Pisti", "Laci", "Józsi", "Másik Laci"]
+
+for names in names:
+    print(f"Kedves {names}! .....")
+    print(".......................")
--- Programozás alapjai/Lesson4/Task1.py
+++
@@ -1,5 +0,0 @@
-names = ["Pisti", "Laci", "Józsi", "Másik Laci"]
-
-for names in names:
-    print(f"Kedves {names}! .....")
-    print(".......................")
