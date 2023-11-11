d82b50800f0aeea09319735f162b0cb4c765ea0f
Ádám <adam@lenartadam.hu>
2023-11-11 01:07:57 +0100

    Update

---
+++ Programozás alapjai/Extra tasks/Task3.py
@@ -0,0 +1,12 @@
+"""
+Szövegelemzés
+
+Egy változóban tárolj el egy bekezdésnyi magyar szöveget.
+Írj egy függvényt, ami eltávolítja az összes írásjelet a sztringből, és a szöveget szavak listájára bontja.
+Segítség: a Python string modulja definiál egy sztring konstanst, ami tartalmazza az írásjeleket (string.punctuation).
+Ezt a listát felhasználva készíts a szövegről statisztikát (írj függvényeket): -
+Hány szó van összesen a szövegben és ennek hány százaléka tartalmaz "e" betűt?
+Melyik szó hányszor fordul elő a szövegben (list.count())?
+A leggyakoribb szavakból készíts szófelhőt!
+"""
+
