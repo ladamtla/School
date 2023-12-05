62709c554e83b4aebcc84a6076f9326c1141f486
ladamtla <adam@lenartadam.hu>
2023-12-05 15:00:25 +0100

    Update

---
+++ sin_cos_calculator.py
@@ -0,0 +1,36 @@
+"Csak a hiba számításhoz"
+import math
+
+def fact(num):
+    fact = 1
+    while num > 0:
+        fact = (fact * num)
+        num -= 1
+    return fact
+
+def sin_cos_calculator(m, x):
+    """
+    :param m: MacLaurin sor fokszáma
+    :param x: szám 
+    :return: szám sinusza, koszinusza
+    """
+    sder = [0, 1, 0, -1]
+    cder = [1, 0, -1, 0]
+    s = 0
+    c = 0
+    se = 0
+
+    while m >= 0:
+        c += (cder[m%4]/fact(m))*x**m
+        s += (sder[m%4]/fact(m))*x**m
+
+        m -= 1
+
+    return s, c
+
+
+m = int(input("Adja meg a MacLaurin sor fokszámát: "))
+x = int(input("Adjon meg egy számot: "))
+sin, cos = sin_cos_calculator(m, x)
+print(f"A {x} szinusza: {round(sin, 10)} - hiba: {abs(math.sin(x)-sin)}")
+print(f"A {x} koszinusza: {round(cos, 10)} - hiba: {abs(math.cos(x)-cos)}")
